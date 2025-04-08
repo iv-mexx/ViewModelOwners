cd4f0a5f76b70e960d7bd167c53c5a1967469493
Markus Chmelar <mexx@innovaptor.com>
2025-04-08 09:31:40 +0200

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "ViewModelOwners",
+    platforms: [
+        .iOS(.v9),
+        .macOS(.v10_10),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "ViewModelOwners",
+            targets: ["ViewModelOwners"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "ViewModelOwners",
+            path: "Sources"
+        ),
+    ]
+)
